98546f65452f07aea60a38fd2a57a9f3c5eb3373
latifii <latifii68@gmail.com>
2023-04-18 00:57:47 +0430

    up

--- src/sections/Faqs.js
+++ src/sections/Faqs.js
@@ -6,7 +6,7 @@ const Faqs = () => {
   return (
     <Wrapper id='faqs'>
       <h2>Frequently Asked Questions</h2>
-      {/* Paragraf */}
+      {/* Paragraf tt  */}
       <p>
         Here are some questions I usually get. Click to toggle the answer, and
         if you still have some more questions, shoot me a message from the
